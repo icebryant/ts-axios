015d32e4e39362a071f18ce58ce055fa9337ed1c
songshaoqing <1103633343@qq.com>
2020-03-06 15:32:03 +0800

    add interceptor

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,7 +1,15 @@
 import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
 import dispatchRequest from './dispatchRequest'
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
@@ -16,14 +24,14 @@ export default class Axios {
   options(url: string, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithoutData('options', url, config)
   }
-  post(url: string,data?:any, config?: AxiosRequestConfig): AxiosPromise {
-    return this._requestMethodWithData('post', url,data, config)
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('post', url, data, config)
   }
-  put(url: string,data?:any, config?: AxiosRequestConfig): AxiosPromise {
-    return this._requestMethodWithData('put', url,data, config)
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('put', url, data, config)
   }
-  patch(url: string,data?:any, config?: AxiosRequestConfig): AxiosPromise {
-    return this._requestMethodWithData('patch', url,data, config)
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('patch', url, data, config)
   }
   _requestMethodWithoutData(
     method: Method,
@@ -40,7 +48,7 @@ export default class Axios {
   _requestMethodWithData(
     method: Method,
     url: string,
-    data?:any,
+    data?: any,
     config?: AxiosRequestConfig
   ): AxiosPromise {
     return this.request(
